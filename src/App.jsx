0ef5363f1afd4193f3c61a2526f1601bf54f34a0
Rolando <rolandohernandezleal@gmail.com>
2026-01-27 12:59:09 -0300

    adding arrow function for now

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,39 @@
 import { useState } from 'react';
 import './App.css'
 
-function Square({value}) {
+function Square({value, onSquareClick}) {
   return (
-    <button className="square">{value}</button>
+    <button className="square" onClick={onSquareClick}>
+      {value}
+    </button>
   );
 }
 
 function Board() {
-  const [squares, setSquare] = useState(Array(9).fill(":)"));
+  const [squares, setSquares] = useState(Array(9).fill(":)"));
+
+  function handleClick(i) {
+    const nextSquares = squares.slice();
+    nextSquares[i] = "X";
+    setSquares(nextSquares);
+  }
 
   return (
     <>
       <div className="board-row">
-        <Square value={squares[0]} />
-        <Square value={squares[1]} />
-        <Square value={squares[2]} />
+        <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
+        <Square value={squares[1]} onSquareClick={() => handleClick(0)} />
+        <Square value={squares[2]} onSquareClick={() => handleClick(0)} />
       </div>
       <div className="board-row">
-        <Square value={squares[3]} />
-        <Square value={squares[4]} />
-        <Square value={squares[5]} />
+        <Square value={squares[3]} onSquareClick={() => handleClick(0)} />
+        <Square value={squares[4]} onSquareClick={() => handleClick(0)} />
+        <Square value={squares[5]} onSquareClick={() => handleClick(0)} />
       </div>
       <div className="board-row">
-        <Square value={squares[6]} />
-        <Square value={squares[7]} />
-        <Square value={squares[8]} />
+        <Square value={squares[6]} onSquareClick={() => handleClick(0)} />
+        <Square value={squares[7]} onSquareClick={() => handleClick(0)} />
+        <Square value={squares[8]} onSquareClick={() => handleClick(0)} />
       </div>
     </>
   );
