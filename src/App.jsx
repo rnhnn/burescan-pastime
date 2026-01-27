3c80a71b15db48161265920dbc59aae26b912b02
Rolando <rolandohernandezleal@gmail.com>
2026-01-27 11:29:57 -0300

    lifting up the state

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,31 @@
 import { useState } from 'react';
 import './App.css'
 
-function Square() {
-  const [value, setValue] = useState(":)");
-
-  function handleClick() {
-    setValue('X');
-  }
-
+function Square({value}) {
   return (
-    <button 
-      className="square"
-      onClick={handleClick}
-    >
-      {value}
-    </button>
+    <button className="square">{value}</button>
   );
 }
 
 function Board() {
+  const [squares, setSquare] = useState(Array(9).fill(":)"));
+
   return (
     <>
       <div className="board-row">
-        <Square />
-        <Square />
-        <Square />
+        <Square value={squares[0]} />
+        <Square value={squares[1]} />
+        <Square value={squares[2]} />
       </div>
       <div className="board-row">
-        <Square />
-        <Square />
-        <Square />
+        <Square value={squares[3]} />
+        <Square value={squares[4]} />
+        <Square value={squares[5]} />
       </div>
       <div className="board-row">
-        <Square />
-        <Square />
-        <Square />
+        <Square value={squares[6]} />
+        <Square value={squares[7]} />
+        <Square value={squares[8]} />
       </div>
     </>
   );
